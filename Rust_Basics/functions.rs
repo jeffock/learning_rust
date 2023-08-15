61f6b1c2ca0f5bb58f21a163e2e3c54dc174940b
jeffock <jeffock1656@gmail.com>
2023-08-15 15:51:15 -0400

    functions

--- Rust_Basics/functions.rs
+++ Rust_Basics/functions.rs
@@ -4,8 +4,37 @@ fn main() {
 
     assert_eq!(s,3);
     println!("Success.");
+
+    print();
+
+    //never_return();
 }
 
 fn sum(x: i32, y: i32) -> i32 {
     x+y
+}
+
+fn print() -> () {
+    println!("Success!");
+}
+
+fn get_option(tp:u8) -> Option<i32> {
+    match tp {
+        1 => {
+            // TODO
+        }
+        _ => {
+            // TODO
+        }
+    };
+
+    never_return()
+}
+
+// diverging function, should never return to caller
+fn never_return() -> ! {
+    // exit immediately
+    unimplemented!()
+    //todo!()
+    //panic!()
 }
